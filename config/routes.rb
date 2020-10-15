2217127b0178a4ce703e4fceaf6061e189f6b15a
郷原秀人 <gochanhidehide@icloud.com>
2020-10-15 21:06:51 +0900

    testdeproy

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :shops
   resources :users
   resources :posts
+  root to: 'home#index'
+  get "home/about" => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
